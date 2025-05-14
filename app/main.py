09d672b8dfa02e2f0ebfaeee901dc5d6bdfd3941
Antonio <jsoliszamacona@gmail.com>
2025-05-14 17:22:55 -0600

    Cambios en los archivos

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.database import engine, Base
 from app.routers import codigos, estados, resumen
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -9,6 +11,10 @@ app = FastAPI(
     description="Consulta de asentamientos por código postal",
     version="1.0.0"
 )
+
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
 
 app.include_router(codigos.router)
 app.include_router(estados.router)
