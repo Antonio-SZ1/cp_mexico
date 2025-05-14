9c646ae838ca1fdf591dac9346fdc5105f1f421c
Antonio <jsoliszamacona@gmail.com>
2025-05-14 14:17:14 -0600

    Actualizacion

--- app/seed.py
+++ app/seed.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.database import engine, SessionLocal, Base
 from app.models import Estado, Municipio, Asentamiento
-from app.crud import (  
+from app.crud import (  # type: ignore
     crear_estado,
     get_estado_por_nombre,
     crear_municipio,
@@ -12,7 +12,7 @@ from app.crud import (
 )
 
 def init_db():
-    # Elimina tablas si existen y las vuelve a crear
+   
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
@@ -20,25 +20,30 @@ def seed_data(csv_path: str):
     db: Session = SessionLocal()
     with open(csv_path, newline="", encoding="utf-8") as f:
         reader = csv.reader(f, delimiter="|")
-        for row in reader:
-            d_estado = row[4].strip()
-            d_mnpio = row[3].strip()
-            d_asenta = row[1].strip()
-            tipo = row[2].strip()
-            zona = row[13].strip()
-            cp = row[0].strip()
+        for idx, row in enumerate(reader, start=1):
+          
+            if not row or len(row) < 14:
+                
+                continue
 
-            # 1. Estado
+            d_estado  = row[4].strip()
+            d_mnpio   = row[3].strip()
+            d_asenta  = row[1].strip()
+            tipo      = row[2].strip()
+            zona      = row[13].strip()
+            cp        = row[0].strip()
+
+         
             estado = get_estado_por_nombre(db, d_estado)
             if not estado:
                 estado = crear_estado(db, d_estado)
 
-            # 2. Municipio
+         
             municipio = get_municipio_por_nombre_estado(db, d_mnpio, estado.id)
             if not municipio:
                 municipio = crear_municipio(db, d_mnpio, estado.id)
 
-            # 3. Asentamiento
+           
             crear_asentamiento(
                 db,
                 nombre=d_asenta,
